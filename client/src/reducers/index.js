6ff086b32010268a1edf26af1562f56338199320
IainLR <iainrobertzon@gmail.com>
2020-12-15 19:22:38 -0600

    dashboard

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 // this entire file is our root reducer 
 import alert from './alert'
 import auth from './auth'
+import profile from './profile'
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    profile
 })
