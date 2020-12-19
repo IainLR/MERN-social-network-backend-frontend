2178ee567e9d1d1c62471db4b24389340d53cf50
IainLR <iainrobertzon@gmail.com>
2020-12-19 17:23:53 -0500

    profile edit form

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layout/Spinner'
+import DashboardActions from './DashboardActions'
 import { Link } from 'react-router-dom'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: {profile, loading }}) => {
@@ -18,7 +19,9 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: {profile, loadi
         <i className="fas fa-user"></i> 
         Welcome { user && user.name}
         </p>
-        {profile !== null ? <Fragment> has </Fragment> : <Fragment>
+        {profile !== null ? <Fragment> 
+            <DashboardActions />
+             </Fragment> : <Fragment>
             <p>You have not yet set up a profile, please add some info</p>
             <Link to='/create-profile' className='btn btn-primary my-1'>Create Profile</Link>
             </Fragment>}
