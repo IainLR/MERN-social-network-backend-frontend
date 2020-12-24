943f1bf87769bee6ac713ead1cab94b55bed77fc
IainLR <iainrobertzon@gmail.com>
2020-12-24 13:26:43 -0500

    like, unlike, delete post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
+import { addLike, removeLike, deletePost } from '../../actions/post'
 
 
-const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments, date} }) => {
+const PostItem = ({ 
+    addLike, 
+    removeLike, 
+    deletePost,
+    auth, post: {_id, text, name, avatar, user, likes, comments, date} }) => {
     return (
-        <div class="post bg-white p-1 my-1">
+        <div className="post bg-white p-1 my-1">
           <div>
             <a href="profile.html">
               <img
-                class="round-img"
+                className="round-img"
                 src={avatar}
                 alt=""
               />
@@ -19,31 +24,33 @@ const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments,
             </a>
           </div>
           <div>
-            <p class="my-1">
+            <p className="my-1">
               {text}
             </p>
-             <p class="post-date">
+             <p className="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-up"></i>{' '}
+            <button onClick={() => addLike(_id)}
+            type="button" className="btn btn-light">
+              <i className="fas fa-thumbs-up"></i>{' '}
               {likes.length > 0 && (<span>{ likes.length }</span>)}
             </button>
-            <button type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-down"></i>
+            <button onClick={() => removeLike(_id)}
+            type="button" className="btn btn-light">
+              <i className="fas fa-thumbs-down"></i>
             </button>
-            <Link to={`/post/${_id}`} class="btn btn-primary">
+            <Link to={`/post/${_id}`} className="btn btn-primary">
               Discussion {comments.length > 0 && (
-              <span class='comment-count'>{ comments.length }</span>
+              <span className='comment-count'>{ comments.length }</span>
               )}
               
             </Link>
             {!auth.loading && user === auth.user._id && (
-                <button      
+                <button onClick={() => deletePost(_id)}     
                 type="button"
-                class="btn btn-danger"
+                className="btn btn-danger"
                 >
-                <i class="fas fa-times"></i>
+                <i className="fas fa-times"></i>
                 </button>
             )}
           </div>
@@ -52,6 +59,9 @@ const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments,
 }
 
 PostItem.propTypes = {
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
 }
@@ -60,4 +70,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem)
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,8 @@
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST
 } from '../actions/types'
 
 const initialState = {
@@ -9,7 +11,7 @@ const initialState = {
     loading: true,
     error: {}
 }
-
+// eslint-disable-next-line
 export default function(state = initialState, action) {
     const { type, payload } = action
 
@@ -20,10 +22,22 @@ export default function(state = initialState, action) {
                 posts: payload,
                 loading: false
             }
+        case DELETE_POST: 
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== payload),
+                loading: false
+            }
         case POST_ERROR:
             return {
                 ...state,
                 error: payload,
+                loading: false
+            }
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === payload.id ? {...post, likes: payload.likes} : post),
                 loading: false
             }
         default:
